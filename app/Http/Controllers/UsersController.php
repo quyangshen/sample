0947479694b3391400beb3ab2afae9f67289578c
vagrant <vagrant@homestead>
2017-07-11 12:28:03 +0000

    unFinish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,12 +6,26 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use App\Models\User;
 class UsersController extends Controller
 {
     //
     public function create()
     {
         return view('users.create');
+    }
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('users.show', compact('user'));
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required'
+        ]);
+        return;
     }
 }
